a8c622f7715bdb80707c2062470fa771d58d201c
vivek vijaykumar <vivekvvk@gmail.com>
2017-06-28 16:34:31 -0700

    minor

--- random/misc.py
+++ random/misc.py
@@ -190,6 +190,7 @@ def ispermute(str1, str2):
     return True
 
 
+#facebook
 def powerset(input_list):
     l = len(input_list)
     mask = 1<<l
@@ -205,3 +206,51 @@ def powerset(input_list):
             i >>= 1
         output_list.append(temp_list)
     return output_list
+
+
+#rubrik
+def str_divide(num1, num2):
+    rlist=[]
+    qlist=[]
+
+    quotient = num1 / num2
+    reminder = num1 % num2
+    qlist.append(quotient)
+
+    if(reminder == 0):
+        return str(quotient)
+    else:
+        #loop for new quotients.
+        while(True):
+            if(reminder < num2):
+                num1 = reminder * 10
+                quotient = num1 / num2
+                reminder = num1 % num2
+                qlist.append(quotient)
+                if(reminder == 0):
+                    #we know its done at this point
+                    break
+                elif(reminder in rlist):
+                    break
+                else:
+                    rlist.append(reminder)
+
+        print qlist, rlist
+        out_str=""
+        if(reminder == 0):
+            out_str = str(qlist[0]) + "." + str(qlist[1:])
+        else:
+            #reminder is valid and it repeated
+            #find the position of repeatition and then move so
+            #many places on the qlist and mark it repeated.
+            pos = 0
+            for x in rlist:
+                if(reminder == x):
+                    break
+                pos += 1
+            if(pos == 0):
+                out_str = str(qlist[0]) + "." + "(" + str(qlist[pos+1:]) + ")"
+            else:
+                out_str = str(qlist[0]) + "." + str(qlist[1:pos]) + "(" + str(qlist[pos:]) + ")"
+
+        return out_str
